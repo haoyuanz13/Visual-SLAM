313c0463e915191f5ca57b46827a9c50e1fada33
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-02-09 18:53:17 -0500

    Update dijkstra.m

--- Trajectory Generation/dijkstra.m
+++ Trajectory Generation/dijkstra.m
@@ -31,7 +31,7 @@ vg = map.xyzToSub(goal);
 
 % calculate the total number of nodes in the map using map.occgrid
 map_shape = size(map.occgrid);
-n_x = map_shape[2]; n_y = map_shape[1]; n_z = map_shape[3];
+n_x = map_shape(1); n_y = map_shape(2); n_z = map_shape(3);
 total_num_nodes = n_x * n_y * n_z;
 
 % calculate the order of the start and the terminal node (x:1st domain; y:2nd domain; z:3rd domain)
@@ -43,14 +43,14 @@ xx = 1 : 1 : n_x;
 yy = 1 : 1 : n_y;
 zz = 1 : 1 : n_z;
 
-[X, Y, Z] = meshgrid(xx, yy, zz);  %% X is column index; Y is row index
+[Y, X, Z] = meshgrid(yy, xx, zz);  %% X is row index; Y is column index
 % heuristic array
 h = zeros(total_num_nodes, 1);
-for i = 1 : n_z:
+for i = 1 : n_z
   % obtain ijk matrix (N x 3)
-  ijk_cur(:, :, 1) = xx(:, :, 1);
-  ijk_cur(:, :, 2) = yy(:, :, 1);
-  ijk_cur(:, :, 3) = zz(:, :, i);
+  ijk_cur(:, :, 1) = X(:, :, 1);
+  ijk_cur(:, :, 2) = Y(:, :, 1);
+  ijk_cur(:, :, 3) = Z(:, :, i);
   ijk_cur_unroll = reshape(ijk_cur, [n_x * n_y, 3]);
 
   % obtain its corresponding orders
@@ -75,31 +75,74 @@ g_dist_vs2v = inf(total_num_nodes, 1); % q array to store the min distance from 
 p_preNode = (1 : 1 : total_num_nodes)'; % p array to store the previous node of current node
 
 % initialize for the start node
-g_dist_vs2v[order_vs] = 0;
-
+g_dist_vs2v(order_vs) = 0;
 
 % loop when vg hasn't been travsersed yet
-while Q_vis(order_vg) == 0:
+while true
   % obtain the terminal node of shortest path wwithout being visited so far
   [g_u, order_u] = min(g_dist_vs2v, [], 'omitnan');
   
   % label u as visited to store its distance from vs to u
   Q_vis(order_u) = g_u;
   % set g[order_u] = nan, as travsersed
-  g_dist_vs2v[order_u] = NaN;
+  g_dist_vs2v(order_u) = NaN;
 
-  % obtain u's all valid neighbors
-  ijk_u = order2Ijk(order_u, nx, ny);
+  % check whether the current min node is the goal node
+  if order_u == order_vg
+    break;
+  end
 
+  % obtain u's all valid neighbors (not visited, not within block and not outside )
+  ijk_u = order2Ijk(order_u, n_x, n_y);
+  ijk_u_neighbors = findNeighbors(ijk_u, n_x, n_y, n_z);
 
+  % exclude visited nodes
+  order_u_neighbors = ijk2Order(ijk_u_neighbors, n_x, n_y);
+  Q_nei = Q_vis(order_u_neighbors);
+  new_order_u_neighbors = order_u_neighbors(Q_nei == -1);
 
+  % convert neighbors from ijk into xyz (physical space)
+  new_ijk_u_nei = ijk_u_neighbors(Q_nei == -1, :);
+  xyz_u_neighbors = map.subToXYZ(new_ijk_u_nei);
 
+  % check position validation
+  collide_nei = map.collide(xyz_u_neighbors);
+  
+  valid_xyz_u_nei = xyz_u_neighbors(collide_nei == 0, :);
+  valid_ijk_u_nei = new_ijk_u_nei(collide_nei == 0, :);
+  valid_order_u_nei = new_order_u_neighbors(collide_nei == 0);
 
+  % compute distance between u and its valid neighbors
+  xyz_u = map.subToXYZ(ijk_u);
+  dist_u_nei = sqrt(sum((valid_xyz_u_nei - xyz_u) .^ 2, 2));
+  d_vs_u_nei = g_u + dist_u_nei;
 
+  % check distance
+  update_ind = (d_vs_u_nei < g_dist_vs2v(valid_order_u_nei));
+  update_order = valid_order_u_nei(update_ind);
 
+  % update if necessary
+  if ~isempty(update_order)
+    % update previous node
+    p_preNode(update_order) = order_u;
 
+    % update min distance
+    g_dist_vs2v(update_order) = d_vs_u_nei(update_ind);
+
+    % add heuristics if using astar
+    if astar
+      g_dist_vs2v(update_order) = g_dist_vs2v(update_order) + h(update_order);
+    end
+  
+  end
+
+  % update expand numbers
+  num_expanded = num_expanded + 1;
 
 end
+
+end
+
 
 
 %% obtain order index based on the ijk indices (N x 3)
@@ -118,4 +161,53 @@ function [ijk] = order2Ijk(order_cur, nx, ny)
   ii = temp_n - (jj - 1) * nx;
 
   ijk = [ii, jj, kk];
+end
+
+
+%% Obtain neighbors, consider 6 directions up-down-right-left-forward-back
+function [neighbors_ijk] = findNeighbors(ijk, nx, ny, nz)
+  ind = 1;
+  % neighbor upper
+  up = ijk + [0, 0, 1];
+  if up(3) <= nz
+    neighbors_ijk(ind, :) = up;
+    ind = ind + 1;
+  end
+
+  % neighbor down
+  down = ijk + [0, 0, -1];
+  if down(3) >= 1
+    neighbors_ijk(ind, :) = down;
+    ind = ind + 1;
+  end
+
+  % neighbor right
+  right = ijk + [0, 1, 0];
+  if right(2) <= ny
+    neighbors_ijk(ind, :) = right;
+    ind = ind + 1;
+  end
+
+  % neighbor left
+  left = ijk + [0, -1, 0];
+  if left(2) >= 1
+    neighbors_ijk(ind, :) = left;
+    ind = ind + 1;
+  end
+  
+  % neighbor forward
+  forward = ijk + [1, 0, 0];
+  if forward(1) <= nx
+    neighbors_ijk(ind, :) = forward;
+    ind = ind + 1;
+  end
+
+  % neighbor back
+  back = ijk + [-1, 0, 0];
+  if back(1) >= 1
+    neighbors_ijk(ind, :) = back;
+    ind = ind + 1;
+  end
+
+
 end
