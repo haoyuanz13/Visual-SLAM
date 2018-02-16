1c2a631daf5886ce6c5f2e0c58765de3fd39a9f2
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-02-15 19:55:58 -0500

    Update trajectory_generator.m

--- Trajectory Generation/trajectory_generator.m
+++ Trajectory Generation/trajectory_generator.m
@@ -1,67 +1,66 @@
-function [ desired_state ] = trajectory_generator(t, qn, map, path)
+function [desired_state] = trajectory_generator(t, qn, varargin)
 % TRAJECTORY_GENERATOR: Turn a Dijkstra or A* path into a trajectory
+% t: A scalar, specifying inquiry time
 %
-% NOTE: This function would be called with variable number of input
-% arguments. In init_script, it will be called with arguments
-% trajectory_generator([], [], map, path) and later, in test_trajectory,
-% it will be called with only t and qn as arguments, so your code should
-% be able to handle that. This can be done by checking the number of
-% arguments to the function using the "nargin" variable, check the
-% MATLAB documentation for more information.
+% varargin: variable number of input arguments. In the framework,
+% this function will first (and only once!) be called like this:
 %
-% map: The map structure returned by your load_map function
-% path: This is the path returned by your planner (dijkstra function)
+% trajectory_generator([],[], 0, path)
 %
-% desired_state: Contains all the information that is passed to the
-% controller, as in phase 2
+% i.e. map = varargin{1} and path = varargin{2}.
 %
-% It is suggested to use "persistent" variables to store map and path
-% during the initialization call of trajectory_generator, e.g.
-% persistent map0 path0
-% map0 = map;
-% path0 = path;
+% path: A N x 3 matrix where each row is (x, y, z) coordinate of a
+% point in the path. N is the total number of points in the path
+%
+% This is when you compute and store the trajectory.
+%
+% Later it will be called with only t and qn as an argument, at
+% which point you generate the desired state for point t.
+%
 
-persistent map0 path0;
-persistent seg_waypoint;
-persistent seg_dis;
-persistent seg_num;
-persistent t_start;
-persistent t_end;
-persistent path_time;
-persistent seg_flag;
-persistent waypoint;
-persistent waypoint_num;
-persistent path_dis;
-persistent path_flag;
-persistent waypoint_s;
+desired_state = [];
 
-%% Four intact variables case: trajectory_generator([], [], map, path)
-if nargin == 4
-  path0 = cell2mat(path);
-  map0 = map;
+%% define presistent variables, use the "persistent" keyword to keep your trajectory around
+% inbetween function calls
+persistent map;  % map object
+persistent traj_wayps;  % waypoints matrix
+persistent waypoints;  % store all valid waypoints
+persistent num_wayp;   % number of segments
+persistent seg_dist;  % distance for each segment
+persistent seg_durations;  % duration for each segment
+persistent seg_t_start;  % start time stamp for each segment
+persistent seg_t_end;  % terminal time stamp for each segment
+persistent seg_ignore_flag;  % the flag to tell whether the ith segment can be ignored due to its small distance between waypoints
+persistent fused_waypoints;  % fused waypoints which connect all segment with same direction together
+persistent validDist_waypoint;  % fused waypoints which connect all segment without any collsion
 
-  path_num = size(path0, 1);
 
-  vector_ini = (path0(2, :) - path0(1, :)) / norm(path0(2, :) - path0(1, :));
-  former_vector = vector_ini;
+%% the case that intacted four arguments which is handle path information, when called without varargin (isempty(varargin) == true), compute
+% and return the desired state here.
+if ~isempty(varargin)
+  map = varargin{1};
+  traj_wayps = varargin{2};  % N x 3 matrix to store waypoints information (xi, yi, zi)
 
-  seg_waypoint = [];
-  waypoint_temp = [];
-  now_vector = [];
+  num_wayp = size(traj_wayps, 1);
+
+  % initial 3d vector
+  vector_ini = (traj_wayps(2, :) - traj_wayps(1, :)) / norm(traj_wayps(2, :) - traj_wayps(1, :));
+  pre_vector = vector_ini;
+
+  fused_waypoints = [];
   seg_count = 0;
-  count = [];
 
   % the vector of the path, calculate from the first path besides start
-  for i = 1 : path_num - 1
-    now_vector = (path0(i + 1, :) - path0(i, :)) / norm(path0(i + 1, :) - path0(i, :));
+  for i = 2 : num_wayp - 1
+    now_vector = (traj_wayps(i + 1, :) - traj_wayps(i, :)) / norm(traj_wayps(i + 1, :) - traj_wayps(i, :));
     % the current vector is same as previous one, so fuse together
-    if now_vector == former_vector  % big problem
-      former_vector = now_vector;
+    if now_vector == pre_vector  % big problem
+      pre_vector = now_vector;
 
       if seg_count > 9
-        if former_vector == now_vector
-          inf_num = size(seg_waypoint, 1);
-          seg_waypoint(inf_num - seg_count + 2 : inf_num - 1,:) = [];
+        if pre_vector == now_vector
+          inf_num = size(fused_waypoints, 1);
+          fused_waypoints(inf_num - seg_count + 2 : inf_num - 1,:) = [];
           seg_count = 0;
         else
           continue;
@@ -72,167 +71,181 @@ if nargin == 4
      end
     
     else
-     seg_waypoint = [seg_waypoint; path0(i, :)];
-     former_vector = now_vector;
+     fused_waypoints = [fused_waypoints; traj_wayps(i, :)];
+     pre_vector = now_vector;
      seg_count = seg_count + 1;
      
      if now_vector(3) ~= 0
       seg_count = 0;
      end
     end
-    
-    count = [count; seg_count];
   end
 
-
-  % count segment distance
-  seg_waypoint = [path0(1, :); seg_waypoint; path0(end, :)];
-  seg_num = size(seg_waypoint, 1) - 1;
-  seg_dis = [];
-  seg_flag = [];
-  for i = 1 : seg_num
-    seg_distance = norm(seg_waypoint(i + 1, :) - seg_waypoint(i, :));
-    
-    if seg_distance < 1.0
-      flag = 1;
-    else
-      flag = 0;
-    end
-
-    seg_dis = [seg_dis; seg_distance];
-    seg_flag = [seg_flag; flag];
-  end
+  % add two-side waypoints into the fused_waypoints set
+  fused_waypoints = [traj_wayps(1, :); fused_waypoints; traj_wayps(end, :)];
+  
+  % update the number of fused waypoints
+  num_wayp = size(fused_waypoints, 1);
 
   % count waypoints: remove waypoints that are too close to any of other
-  waypoint = [];
-  for i = 1 : seg_num - 1
-    if norm(seg_waypoint(i + 1, :) - seg_waypoint(i, :)) > 0.1
-      waypoint = [waypoint; seg_waypoint(i,:)];
+  validDist_waypoint = [];
+  for i = 1 : num_wayp - 2
+    if norm(fused_waypoints(i + 1, :) - fused_waypoints(i, :)) > 0.1
+      validDist_waypoint = [validDist_waypoint; fused_waypoints(i, :)];
     else
       continue;
     end
   end
-  waypoint = [waypoint; path0(end,:)];
+  validDist_waypoint = [validDist_waypoint; traj_wayps(end,:)];
 
-  % check collision state of waypoints
-  num = size(waypoint, 1);
-  div = 10;
-  waypoint_s = waypoint(1, :);
-  waypoint_final = [];
-  check = [];
-  for i = 1 : num - 1
-    length = waypoint(i + 1,: ) - waypoint_s;
-    delt = length / div;
+
+  % % check collision state of waypoints
+  % num_wayp = size(validDist_waypoint, 1);
+  % div = 10;
+
+  % wp_start = validDist_waypoint(1, :);
+  % waypoint_temp = [];
+  % for i = 1 : num_wayp - 1
+  %   len_interval = validDist_waypoint(i + 1,: ) - wp_start;
+  %   delta_len = len_interval / div;
     
-    points = [];
-    points = [waypoint_s(1, :) + delt; waypoint_s(1, :) + 2 * delt; waypoint_s(1, :) + 3 * delt; ...
-              waypoint_s(1, :) + 4 * delt; waypoint_s(1, :) + 5 * delt; waypoint_s(1, :) + 6 * delt; ...
-              waypoint_s(1, :) + 7 * delt; waypoint_s(1, :) + 8 * delt; waypoint_s(1, :) + 9 * delt; ...
-              waypoint_s(1, :) + delt];
+  %   % creata a list of points along two waypoints to check the collsion state
+  %   points = [wp_start + delta_len;       wp_start + 2 * delta_len;   wp_start + 3 * delta_len; ...
+  %             wp_start + 4 * delta_len;   wp_start + 5 * delta_len;   wp_start + 6 * delta_len; ...
+  %             wp_start + 7 * delta_len;   wp_start + 8 * delta_len;   wp_start + 9 * delta_len; wp_start + 10 * delta_len];
 
-    check = collide(map0, points);
-    % all waypoints are safe
-    if check == zeros(10, 1)
-      continue;
-    else
-      waypoint_temp = waypoint(i, :);
-      waypoint_final = [waypoint_final; waypoint_temp];
-      waypoint_s = waypoint(i, :);
-    end
-  end
-
-
-  waypoint = [];
-  waypoint = [path0(1, :); waypoint_final; path0(end, :)];
-  waypoint_num = size(waypoint, 1) - 1;
-  path_dis = [];
-  path_flag = [];
-  for i = 1 : waypoint_num
-    path_distance = norm(waypoint(i + 1, :) - waypoint(i, :));
+  %   check = map.collide(points);
+    
+  %   % all longside waypoints are safe such that we can fuse two waypoints together
+  %   if check == zeros(10, 1)
+  %     continue;
+    
+  %   % if there exits collision, append it into the final waypoints set
+  %   else
+  %     waypoint_temp = [waypoint_temp; validDist_waypoint(i, :)];
+  %     wp_start = validDist_waypoint(i, :);
+  %   end
+  % end
+  % % the final updated waypoints set
+  % waypoints = [traj_wayps(1, :); waypoint_temp; traj_wayps(end, :)];
+  
+  waypoints = validDist_waypoint;
+  % obtain distance for each segment 
+  num_wayp = size(waypoints, 1);
+  seg_dist = [];
+  seg_ignore_flag = [];
+  for i = 1 : num_wayp - 1
+    path_distance = norm(waypoints(i + 1, :) - waypoints(i, :));
     if path_distance < 1.0
       flag = 1;
     else
       flag = 0;
     end
-    path_dis = [path_dis; path_distance];
-    path_flag = [path_flag; flag];
+    seg_dist = [seg_dist; path_distance];
+    seg_ignore_flag = [seg_ignore_flag; flag];
   end
 
-
-%% Two input variables case for test: trajectory_generator(t, qn)
-else
+  % calculate segment duration and time stamps
   t_s = 0;
-  t_start = [];
-  t_end = [];
-  path_time = [];
-  % assign segment times
-  for i = 1 : waypoint_num
-    if path_dis(i) < 1
-      seg_t = path_dis(i) / 1;
-    elseif 1 <= path_dis(i) && path_dis(i) <= 5
-      seg_t = path_dis(i) / 2;
-    else 
-      seg_t = path_dis(i) / 2.5;
+  seg_t_start = [];
+  seg_t_end = [];
+  seg_durations = [];
+
+  for i = 1 : num_wayp - 1
+    % try to average durations
+    if seg_dist(i) < 1
+      seg_cur_t = seg_dist(i);
+    elseif (seg_dist(i) >= 1) && (seg_dist(i) < 5)
+      seg_cur_t = seg_dist(i) / 2;
+    else
+      seg_cur_t = seg_dist(i) / 2.5;
     end
-    
-    path_time = [path_time; seg_t];
-    t_f = t_s + seg_t;
-    t_end = [t_end; t_f];
-    t_start = [t_start; t_s];
-    t_s = t_f;
+
+    seg_durations = [seg_durations; seg_cur_t];
+    seg_t_start = [seg_t_start; t_s];
+
+    % update the start time stamp for each segment
+    t_s = t_s + seg_cur_t;
+    seg_t_end = [seg_t_end; t_s];
   end
-  
-  % trajectory processing if t < t_end
-  if t <= t_end(end)    
-    for i = 1 : waypoint_num
-      % locate in the ith segment and segment distance is larger than 1
-      if t_start(i) <= t && t < t_end(i) && path_flag(i) == 0
-        ts = 0;
-        t = t - t_start(i);
-        tf = ts + path_time(i);
-        T = [1  ts  ts ^ 2    ts ^ 3        ts ^ 4        ts ^ 5;
-             0   1  2 * ts    3 * ts ^ 2    4 * ts ^ 3    5 * ts ^ 4;
-             0   0       2    6 * ts        12 * ts ^ 2   20 * ts ^ 3;
-             1  tf  tf ^ 2    tf ^ 3        tf ^ 4        tf ^ 5;
-             0   1  2 * tf    3 * tf ^ 2    4 * tf ^ 3    5 * tf ^ 4;
-             0   0       2    6 * tf        12 * tf ^ 2   20 * tf ^ 3];           
-        
-        conditions_x = [waypoint(i, 1) 0 0 waypoint(i + 1, 1) 0 0]';
-        coeffs_x = T \ conditions_x;
-        pos_x = coeffs_x' * [1;  t;  t ^ 2;      t ^ 3;       t ^ 4;       t ^ 5];
-        vel_x = coeffs_x' * [0;  1;  2 * t;  3 * t ^ 2;   4 * t ^ 3;   5 * t ^ 4];
-        acc_x = coeffs_x' * [0;  0;      2;      6 * t;  12 * t ^ 2;  20 * t ^ 3];
 
-        conditions_y = [waypoint(i, 2) 0 0 waypoint(i + 1, 2) 0 0]';
-        coeffs_y = T \ conditions_y;
-        pos_y = coeffs_y' * [1;  t;  t ^ 2;      t ^ 3;       t ^ 4;       t ^ 5];
-        vel_y = coeffs_y' * [0;  1;  2 * t;  3 * t ^ 2;   4 * t ^ 3;   5 * t ^ 4];
-        acc_y = coeffs_y' * [0;  0;      2;      6 * t;  12 * t ^ 2;  20 * t ^ 3];
-        
-        conditions_z = [waypoint(i, 3) 0 0 waypoint(i + 1, 3) 0 0]';
-        coeffs_z = T \ conditions_z;
-        pos_z = coeffs_z' * [1;  t;  t ^ 2;      t ^ 3;       t ^ 4;       t ^ 5];
-        vel_z = coeffs_z' * [0;  1;  2 * t;  3 * t ^ 2;   4 * t ^ 3;   5 * t ^ 4];
-        acc_z = coeffs_z' * [0;  0;      2;      6 * t;  12 * t ^ 2;  20 * t ^ 3];
 
-        pos = [pos_x; pos_y; pos_z];
-        vel = [vel_x; vel_y; vel_z];
-        acc = [acc_x; acc_y; acc_z];
+%% the case that only two input arguments which is for testing
+else
+  % Process trajectory when t < terminal time stamp
+  if t <= seg_t_end(end)
+    % determine the segment it locates in
+    for i = 1 : num_wayp - 1
+      if (seg_t_start(i) <= t) && (t < seg_t_end(i))
+        ind_seg = i;
+        break;
+      end
+    end
+
+    % obtain the corresponding two-sides waypoints
+    wp_s = waypoints(ind_seg, :);
+    wp_e = waypoints(ind_seg + 1, :);
+
+    % if the current segment is too short, use waypoints directly
+    if seg_ignore_flag(ind_seg) == 1
+      pos = wp_e';
+      vel = [0; 0; 0];
+      acc = [0; 0; 0];
+    
+    % generate polynomial trajectory and determine its state
+    else
+      t_0 = 0;  % the initial time stamp
+      t_f = seg_durations(ind_seg); % the terminal time stamp
+      t = t - seg_t_start(ind_seg);  % remove the offset in the current segment
+
+      Coeff_0 = [1, t_0, t_0^2, t_0^3, t_0^4, t_0^5;
+                 0, 1, 2 * t_0, 3 * t_0^2, 4 * t_0^3, 5 * t_0^4;
+                 0, 0, 2, 6 * t_0, 12 * t_0^2, 20 * t_0^3];
+             
+      Coeff_f = [1, t_f, t_f^2, t_f^3, t_f^4, t_f^5;
+                 0, 1, 2 * t_f, 3 * t_f^2, 4 * t_f^3, 5 * t_f^4;
+                 0, 0, 2, 6 * t_f, 12 * t_f^2, 20 * t_f^3];
+
+      Coeff = [Coeff_0; Coeff_f];
+
+      %% Compute the Polynomial trajectory in angle domain
+
+      % define the initial and terminal states in x domain
+      b_x = [wp_s(1); 0; 0; wp_e(1); 0; 0];
+      a_x = Coeff \ b_x;
       
-      % locate in the ith segment and segment distance is samller than 1, s.t. no need to compute polynomial
-      elseif t_start(i) <= t && t < t_end(i) && path_flag(i) == 1
-        pos = waypoint(i + 1, :);
-        vel = [0 0 0];
-        acc = [0 0 0];
+      pos_x = a_x' * [1; t; t^2; t^3; t^4; t^5];
+      vel_x = a_x' * [0; 1; 2 * t; 3 * t^2; 4 * t^3; 5 * t^4];
+      acc_x = a_x' * [0; 0; 2; 6 * t; 12 * t^2; 20 * t^3];
+
+      % define the initial and terminal states in y domain
+      b_y = [wp_s(2); 0; 0; wp_e(2); 0; 0];
+      a_y = Coeff \ b_y;
       
-      end 
+      pos_y = a_y' * [1; t; t^2; t^3; t^4; t^5];
+      vel_y = a_y' * [0; 1; 2 * t; 3 * t^2; 4 * t^3; 5 * t^4];
+      acc_y = a_y' * [0; 0; 2; 6 * t; 12 * t^2; 20 * t^3];
+
+      % define the initial and terminal states in z domain
+      b_z = [wp_s(3); 0; 0; wp_e(3); 0; 0];
+      a_z = Coeff \ b_z;
+      
+      pos_z = a_z' * [1; t; t^2; t^3; t^4; t^5];
+      vel_z = a_z' * [0; 1; 2 * t; 3 * t^2; 4 * t^3; 5 * t^4];
+      acc_z = a_z' * [0; 0; 2; 6 * t; 12 * t^2; 20 * t^3];
+
+     %% Assign return variables
+      pos = [pos_x; pos_y; pos_z];
+      vel = [vel_x; vel_y; vel_z];
+      acc = [acc_x; acc_y; acc_z];
     end
   
-  % terminal state
-  else 
-    pos = [waypoint(end, 1); waypoint(end, 2); waypoint(end, 3)];
+
+  % The terminal static state
+  else
+    pos = [waypoints(end, 1); waypoints(end, 2); waypoints(end, 3)];
     vel = [0; 0; 0];
-    acc = [0; 0; 0]; 
+    acc = [0; 0; 0];
   end
 
   desired_state.pos = pos(:);
@@ -242,4 +255,4 @@ else
   desired_state.yawdot = 0;
 end
 
-end 
+end
