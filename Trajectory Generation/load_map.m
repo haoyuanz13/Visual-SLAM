08ba44d3472f99d4d69dc7b11d0c110dde97cd92
Haoyuan(Steve) Zhang <haoyuanz@seas.upenn.edu>
2018-03-10 15:19:50 -0500

    Delete load_map.m

--- Trajectory Generation/load_map.m
+++
@@ -1,6 +0,0 @@
-function map = load_map(filename, res_xy, res_z, margin)
-% LOAD_MAP Load a map from disk.
-%  MAP = LOAD_MAP(filename, res_xy, res_z).  Creates an occupancy grid map
-%  which represents obstacles and .
-map = OccMap.LoadMap(filename, res_xy, res_z, margin);
-end
